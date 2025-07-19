4faea8f8db6f2750218306b1e5edd7f05b59efe7
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-19 09:35:13 +0330

    add new venv and connect project to db

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_shop',
+        'USER': 'root',
+        'PASSWORD': 'Amir1380',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
