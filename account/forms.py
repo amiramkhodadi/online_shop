7894133fd8301d6370d105654294f70ffdb7b761
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-19 20:02:45 +0330

    add validator for login form

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 class LoginForm(forms.Form):
-    phone =forms.CharField(max_length=11 , widget=forms.TextInput(attrs={'class':'form-control'}))
+            # dar mord mabhase validator ha mitonim ham b shekl pain inaro ba estefade az validator  haye khode jango estefade konim v agr yekam khas tar bashnd mitoniom ba estefade az neveshtan tabe clean braye hame v ya braye tak feild validation error on feild ro baresi konim
+    phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs={'class': 'form-control'}) , validators=[MaxLengthValidator(11),MinLengthValidator(11)])
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+
