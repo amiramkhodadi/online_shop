e34cf14e8c2e80fc2d634b8659b082e37cfd5dae
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-21 14:54:02 +0330

    add cart detail template for render

--- config/urls.py
+++ config/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('account/', include('account.urls')),
     path('product/', include('product.urls')),
+    path('cart/', include('cart.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
