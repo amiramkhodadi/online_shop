108cb5e8c3267dfd88d63051a45364cb4ec3e961
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-24 08:56:32 +0330

    customize admin file for orders

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . models import Order , OrderItem
 
-# Register your models here.
+
+
+class OrderItemsAdmin(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+   list_display = ('user' , 'address' , 'phone' , 'is_paid')
+   inlines = (OrderItemsAdmin, )
+   list_filter = ('is_paid' , )
+
+
+
+
