0a65f163b8622c2c52c10c84bf1d21d36fcb3eda
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-24 08:46:19 +0330

    create model for order and order items

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+from account.models import User
+from product.models import Product
+
+
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE , related_name='orders')
+    address = models.CharField(max_length=100)
+    email = models.EmailField( null=True, blank=True)
+    phone = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f'{self.user.full_name} - {self.phone} - {self.is_paid}'
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE , related_name='items')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE , related_name='items')
+    color = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+
+
+
+    def __str__(self):
+        return f'{self.product.title} - {self.color} - {self.size} - {self.price}'
