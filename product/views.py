d405e754260f262f968858b6f277365e7f2068a9
Amir khodadi <amiramkhodadi@gmail.com>
2025-08-03 13:12:44 +0330

    add filter for products list

--- product/views.py
+++ product/views.py
@@ -23,3 +23,19 @@ class ProductListView(ListView):
     template_name = 'product/products_list.html'
     paginate_by = 1
 
+    def get_context_data(self, **kwargs):
+        colors = self.request.GET.getlist('color')
+        sizes = self.request.GET.getlist('size')
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+        context =super(ProductListView, self).get_context_data()
+        print(colors, sizes, min_price, max_price)
+        queryset = Product.objects.all()
+        if colors :
+            queryset = queryset.filter(color__title__in=colors)
+        if sizes :
+            queryset = queryset.filter(size__title__in=sizes)
+        if min_price and max_price :
+            queryset = queryset.filter(price__gte=min_price, price__lte=max_price)
+        context['products'] = queryset
+        return context
